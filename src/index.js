75241f35cc593f7348b6a204e4ee0765623d687c
Yann <mail@it.yann.red>
2020-04-29 16:53:15 +0200

    Premier commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import StartGame from './startGame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StartGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/startGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './startGame.css';
+
+function StartGame() {
+  return (
+    <div className="startGame">
+      <button>Commencer un jeu</button>
+    </div>
+  );
+}
+
+export default StartGame;
